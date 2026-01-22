2f37ba9b80a7e3f8497b97247fadc029b5b886a8
Brian <brianyin16@gmail.com>
2026-01-22 09:29:39 -0500

    Quartz sync: Jan 22, 2026, 9:29 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,7 +8,7 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
+      LinkedIn: "https://www.linkedin.com/brianyin2",
       "Discord Community": "https://discord.gg/cRFFHYye7t",
     },
   }),
